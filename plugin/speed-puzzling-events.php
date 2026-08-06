3782b19bb52d9b5952a4386d02b82fcd830aa709
peddle2themetal <melanie.peddle@gmail.com>
2026-08-05 21:36:50 -0600

    Create initial plugin structure

---
+++ plugin/speed-puzzling-events.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Speed Puzzling Events
+ * Plugin URI: https://speedpuzzling.events
+ * Description: Core functionality for SpeedPuzzling.Events
+ * Version: 0.0.1
+ * Author: Mel Rex
+ * License: GPL-2.0-or-later
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
